a6ba415610d97954c2ad0610445ce1a4261e6149
cirex-peroche <cirex_peroche@yahoo.com>
2025-06-13 14:14:34 -0400

    Update main.js

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,6 @@
 var A = "This is a string using variable";
+var B = "This alert is from my main.js using window.alert! variable B";
 alert("This alert is from my main.js using alert!");
-window.alert ("This alert is from my main.js using window.alert!"); 
-document.write (A);
+window.alert (B); 
+document.write (A + "<br>");
+document.write ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
