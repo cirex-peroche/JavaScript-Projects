c49b83796db9a57c574982f79ebc8e7c9677777a
cirex-peroche <cirex_peroche@yahoo.com>
2025-06-14 00:36:39 -0400

    updates

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,21 @@
-var A = "This is a string using variable";
-var B = "This alert is from my main.js using window.alert! variable B";
-alert("This alert is from my main.js using alert!");
-window.alert (B); 
+var A = "This is a string using variable.";
+var B = "\"This alert is from my main.js using window.alert! variable B.\"";
+var C = A + " " + B;
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+var Sent1 = "This is the beginning of the string, ";
+var Sent2 = "and this is the end of the string.";
+
+//alert("This alert is from my main.js using alert!");
+//window.alert (B); 
+
 document.write (A + "<br>");
-document.write ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write ("\"Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded." + "<br>");
+document.write (C + "<br>");
+document.write (Dad + "<br>");
+document.write (3 + 3 + " " + "\"This is an expression using addition.\"<br>");
+document.write (Sent1 + Sent2 + "<br>");
+
+function My_First_Function () {
+var str = "This is the button text!";
+document.getElementById ("Button_Text") . innerHTML = str;
+}
