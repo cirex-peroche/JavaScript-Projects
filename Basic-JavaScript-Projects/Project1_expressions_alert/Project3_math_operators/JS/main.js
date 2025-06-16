38eba9004af347a4c66208adb50a8db434a8c8ad
cirex-peroche <cirex_peroche@yahoo.com>
2025-06-16 13:02:08 -0400

    test

--- Basic-JavaScript-Projects/Project1_expressions_alert/Project3_math_operators/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/Project3_math_operators/JS/main.js
@@ -1,3 +1,32 @@
-document.write (3 + 3 + " " + "\"This is an expression using addition.\"<br>");
+function addition() {
+    let addition = 5 + 3;  // Example addition
+    document.getElementById("addition").innerHTML = "5 + 3 = " + addition;
+}
 
+function subtraction() {
+    var Subtraction = 5 - 2;
+    document.getElementById ("subtraction") . innerHTML = "5 - 2 = " + Subtraction;
 
+}
+
+function multiplication() {
+    var multiplication = 5 * 2;
+    document.getElementById ("multiplication") . innerHTML = "5 * 2 = " + multiplication;
+
+}
+
+function division() {
+    var division = 15 / 3;
+    document.getElementById ("division") . innerHTML = "15 / 3 = " + division;
+
+}
+
+function Math() {
+    var Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById ("Math") . innerHTML = "(1 + 2) * 10 / 2 -5 = " + Math;
+}
+
+function remainder() {
+    var remainder = 25 % 6;
+    document.getElementById ("remainder") . innerHTML = "When you divide 25 by 6 you have a remainder of: " + remainder;
+}
